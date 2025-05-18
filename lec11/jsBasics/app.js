e2fe7f0507633231b0f1509c54bed38584ae53c9
prakash70428 <prakashmendish@gmail.com>
2025-05-19 00:39:13 +0530

    lecture 10 updated

---
+++ lec11/jsBasics/app.js
@@ -0,0 +1,12 @@
+// let myname = "utkarsh";
+// console.log(myname);
+
+let firstName = 'prakash';
+let secondName = 'kumar';
+
+// let fullName = firstName + secondName;
+// console.log(fullName)
+
+let greet = `Hi ${firstName} ${secondName} , How are you ?`
+
+console.log(greet)
---
+++ lec11/jsBasics2/app.js
@@ -0,0 +1,28 @@
+// let str = "hi i am prakash";
+// console.log(str);
+
+// let magicNum = 10;
+// // magicNum = 'praky';
+
+// console.log(typeof(magicNum));
+
+// let naam = 10;
+// naam = true;
+// console.log(naam);
+// naam = 'prakash';
+// console.log(naam);
+
+// let str = 'utkarsh';
+// console.log(str[200]);
+
+// let str;
+// str = 'prakashkumar.india@gmail.com'
+// console.log(str);
+
+// let fname = 'utkarsh\nkumar';
+// console.log(fname);
+let str = 'PRAKASH KUMAR'
+
+console.log(str.substr(2));
+console.log(str.substr(2,5));
+console.log(str.substr(-5,5));
