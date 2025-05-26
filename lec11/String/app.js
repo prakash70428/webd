e287e432abecdcfee17d1d40b8340cd44ed71f0b
prakash70428 <prakashmendish@gmail.com>
2025-05-26 23:58:05 +0530

    lecture 11 updated

---
+++ lec11/String/app.js
@@ -0,0 +1,32 @@
+let str = 'prakashkumar.india@gmail.com'
+console.log(str);
+
+//1.substr method
+// console.log(str.substr(2))
+// console.log(str.substr(2,5))
+// console.log(str.substr(-5,6))
+// console.log(str.substr(-5,-6))
+
+// 2.substring method
+// console.log(str.substring(3));
+// console.log(str.substring(2,7));
+// console.log(str.substring(7,2));
+// console.log(str.substring(-5,2));
+// console.log(str.substring(-5,-2));
+
+//3. indexOf
+// console.log(str.indexOf('a'));
+// console.log(str.indexOf('s'));
+// console.log(str.indexOf('a',3));
+
+//3rd encounter wala problem
+// console.log(str.indexOf('a')); //1st encounter
+// console.log(str.indexOf('a' , str.indexOf('a') + 1)); //2nd enocounter
+// console.log(str.indexOf('a' , str.indexOf('a' , str.indexOf('a') + 1) + 1)) //3rd encounter
+
+// replace
+// console.log(str.replace('a','u'))
+
+//replaceAll
+// console.log(str.replaceAll('a','#'));
+
