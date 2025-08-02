2b43b74f490223032c3cb27549d41ddfa2fce773
prakash70428 <prakashmendish@gmail.com>
2025-08-02 17:00:32 +0530

    lecture 16 updated

--- lec16/this/app.js
+++ lec16/this/app.js
@@ -26,14 +26,48 @@
 // myFun();
 
 //question 2 :
-let obj2 = {
-    num : 20,
+// let obj2 = {
+//     num : 20,
+//     fn : function(){
+//             function calculate(){
+//                 console.log(this);
+//             }
+//             calculate();
+//         }
+// }
+
+// obj2.fn();
+
+//3. constructor function calling
+// function CreateObj(){
+//     this.x = 20;
+// }
+
+// let obj = new CreateObj();
+// console.log(obj);
+// console.log(obj.x);
+//newly created object ko this point krega hamesha
+
+//4. .call() , .apply() , .bind()
+let obj = {
+    a : 20,
     fn : function(){
-            function calculate(){
-                console.log(this);
-            }
-            calculate();
-        }
+        console.log(this);
+    }
 }
 
-obj2.fn();
+let obj2 = {
+    a : 50   
+}
+
+// obj.fn();
+// obj2.fn(); //error
+
+obj.fn.call(obj2);
+
+//jab bhi tum .call() method ko istemaal kr rahe hote ho tab jo aap  call ke andar
+// mention karte ho aapka this usse point karta hai.
+
+
+
+
