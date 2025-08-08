3ec929d446a7c6078bdaac2b2d6b4a38be2f3307
prakash70428 <prakashmendish@gmail.com>
2025-08-08 13:26:15 +0530

    lecture 16 updated

--- lec16/this/app.js
+++ lec16/this/app.js
@@ -49,25 +49,53 @@
 //newly created object ko this point krega hamesha
 
 //4. .call() , .apply() , .bind()
-let obj = {
-    a : 20,
-    fn : function(){
-        console.log(this);
-    }
-}
+// let obj = {
+//     a : 20,
+//     fn : function(a,b,c){
+//         console.log(this ,  a, b, c);
+//     }
+// }
 
-let obj2 = {
-    a : 50   
-}
+// let obj2 = {
+//     a : 50   
+// }
+
+// let obj3 = {
+//     name : 'utkarsh'
+// }
 
 // obj.fn();
 // obj2.fn(); //error
 
-obj.fn.call(obj2);
+// obj.fn.call(obj3);
 
 //jab bhi tum .call() method ko istemaal kr rahe hote ho tab jo aap  call ke andar
 // mention karte ho aapka this usse point karta hai.
 
+// obj.fn.call(obj, 1, 2, 3);
+// obj.fn.apply(obj,[5, 6, 7]);
+
+//.bind()
+
+// let obj3 = {
+//     num : 3,
+//     fn : function(){
+//         console.log(this);
+//     }
+// }
+
+// let myFun = obj3.fn.bind(obj3);
+// myFun(); //regular invocation
+
+//5. arrow function
+//traditional function
+function add(a , b){
+    return a +b;
+}
+//Arrow function
+const add = (a,b) => {
+    return a + b;
+}
 
 
 
