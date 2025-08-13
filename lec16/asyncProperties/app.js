35188fd7ac08615d18fa2641fa138d827e9005cb
prakash70428 <prakashmendish@gmail.com>
2025-08-13 21:16:33 +0530

    lecture 16 updated

---
+++ lec16/asyncProperties/app.js
@@ -0,0 +1,26 @@
+// console.log('start');
+// console.log('utkarsh esko run karo after 4 second');
+// console.log('end');
+
+// console.log('start');
+
+// setTimeout(function(){
+//     console.log('utkarsh esko run karo after 4 second');
+// } , 4000)
+
+// console.log('end');
+
+//question:
+
+console.log('start');
+
+setTimeout(function(){
+    console.log('utkarsh esko run karo after 2 second');
+} , 2000)
+
+setTimeout(function(){
+    console.log('utkarsh esko run karo after 4 second');
+} , 4000)
+
+console.log('end');
+
