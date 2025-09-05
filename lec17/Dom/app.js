c1526ee9dea934a2ed6b3788f92f5727a6d2ff1c
prakash70428 <prakashmendish@gmail.com>
2025-09-05 20:32:27 +0530

    lecture 17 updated

---
+++ lec17/Dom/app.js
@@ -0,0 +1,51 @@
+// console.log(document);
+
+//----------------------tagName-----------------------
+// console.log(document.getElementsByTagName('h1'));
+// let h1Arr = document.getElementsByTagName('h1');
+// console.log(h1Arr[0]);
+// console.log(h1Arr[1]);
+
+// loops
+// for(let item of h1Arr){
+//     console.log(item);
+// }
+
+// let para = document.getElementsByTagName('p');
+// for(let item of para){
+//     // console.log(item);
+//     // item.style.color = 'red';
+//     item.style.border = '2px dotted red';
+// }
+
+// ----------------Id--------------------
+// let id = document.getElementById('akash');
+// console.log(id);
+// id.style.fontSize = '60px';
+
+//----------------ClassName---------------
+// let para = document.getElementsByClassName('dom');
+// console.log(para);
+// for(let item of para){
+    // item.style.color = 'orange';
+// }
+
+//-----------------querySelector----------------------
+// let h1 = document.querySelector('h1');
+// console.log(h1);
+
+// let dom = document.querySelector('.dom');
+// console.log(dom);
+
+// let ak = document.querySelector('#akash');
+// console.log(ak);
+
+// -----------------querySelectorAll--------------
+// let h1 = document.querySelectorAll('h1');
+// console.log(h1);
+
+// let dom = document.querySelectorAll('.dom');
+// console.log(dom);
+
+let ak = document.querySelectorAll('#akash');
+console.log(ak);
---
+++ lec17/someProperties/app.js
@@ -0,0 +1,11 @@
+//------innerText------
+let para = document.querySelector('p');
+console.log(para.innerText);
+
+//------textContent----
+let para2 = document.querySelector('p');
+console.log(para2.textContent);
+
+//-----innerHTML-----
+let para3 = document.querySelector('p');
+console.log(para3.innerHTML);
