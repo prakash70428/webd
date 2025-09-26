b8c8c6d899b18c86bc486c15ab6b1687022b8bb2
prakash70428 <prakashmendish@gmail.com>
2025-09-26 20:57:26 +0530

    lecture 18 updated

---
+++ lect-18/nodeBasics/index.js
@@ -0,0 +1,24 @@
+console.log("Hello Utkarsh✌");
+console.log("aaj se ham nodejs mein kaam karengey")
+
+let PI = 3.14;
+let magicNum = 68;
+console.log('utkarsh sab kuch js mein shikha hai');
+
+function sqr(num){
+    return num * num;
+}
+
+let obj = {
+    first : 'utkarsh',
+    last : 'kumar',
+    age : '25',
+    getNaam : function(){
+        console.log(this.first + " " + this.last);
+    }
+}
+
+console.log(PI);
+console.log(magicNum);
+console.log(sqr(6));
+console.log(obj.getNaam());
