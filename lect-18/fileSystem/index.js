502f0c93c3e003b598d40f976e9e413d4db15da7
prakash70428 <prakashmendish@gmail.com>
2025-10-10 15:20:35 +0530

    lecture 18 updated

--- lect-18/fileSystem/index.js
+++ lect-18/fileSystem/index.js
@@ -5,13 +5,33 @@ let fs = require('fs');
 
 //create or write in fs
 let data = 'utkarsh kal tak sab revise karke rakhna and code karke bhi'
+let data2 = 'changed data hu mein'
 
-fs.writeFile('abc.txt',data,{
-    encoding : 'utf-8',
-    flag : 'w'
-} , (err) => {
-    if(err) { throw err }
-    console.log('file written succefully');
-});
+// fs.writeFile('abc.txt',data,{
+//     encoding : 'utf-8',
+//     flag : 'w'
+// } , (err) => {
+//     if(err) { throw err }
+//     console.log('file written succefully');
+// });
 
-// fs.writeFileSync('abc.txt',data);
+// fs.writeFileSync('abc.txt',data2);
+
+//update or appendFile
+// fs.appendFile('abc.txt',
+//     '-- akash barish ho rahi hai',
+//     (err) => {
+//     if(err) { throw err }
+//     console.log('update kardiya hai bhai!');
+// }
+// )
+
+// fs.appendFileSync('abc.txt','  ----ye dekho maine phirse update kardiya');
+
+//Delelte or unlink
+// fs.unlink('abc.txt', (err) => {
+//   if (err) throw err;
+//   console.log('bhai file delete hogyi hai.');
+// });
+
+// fs.unlinkSync('abc.txt');
